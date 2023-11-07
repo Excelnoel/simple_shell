a60059475a289d595b9e3258f9e11ac7a138ca62
Excelnoel <mannymacadex@gmail.com>
2023-11-07 05:22:15 -0800

    getinfo

--- getinfo.c
+++ getinfo.c
@@ -55,7 +55,7 @@ void free_info(info_t *info, int all)
 		if (!info->cmd_buf)
 			free(info->arg);
 		if (info->env)
-			free_list(&(ifo->env));
+			free_list(&(info->env));
 		if (info->history)
 			free_list(&(info->history));
 		if (info->alias)
