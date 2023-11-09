67c951f8b576179af35c63de014cad325c46b5ef
BeautifulAngel246 <favouronuche@gmail.com>
2023-11-10 00:39:31 +0100

    errors.c

--- errors.c
+++ errors.c
@@ -1,77 +1,78 @@
 #include "shell.h"
 /*
- * _eputs - printing an input string
+ * custom_puts - print a string to standard error
  * @str: the string to be printed
- * Return: Nothing
+ * Return: None
  */
-void _eputs(char *str)
-{
-	int i = 0;
+void custom_puts(char *str) {
+
+	int index = 0;
 
 	if (!str)
 		return;
-	while (str[i] != '\0')
-	{
-	_eputchar(str[i]);
-	i++;
-	}
-}
-/**
- * _eputchar - writes the character c to stderr
- * @c: The character to print
- * Return: On success 1
- * On error, -1 is returned, and errno is set appropriately
- */
-int _eputchar(char c)
-{
-	static int i;
-	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-	{
-		write(2, buf, i);
-		i = 0;
+	while (str[index] != '\0') {
+	custom_putchar(str[index]);
+	index++;
 	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
 }
 /**
- * _putfd - writes the character c to given fd
+ * custom_putchar - Write a character to standard error
+ * @c: The character to print
+ * Return: 1 on success, -1 on error
+ */
+int custom_putchar(char c) {
+
+	static int buffer_index;
+	static char output_buffer[OUTPUT_BUFFER_SIZE];
+
+	if (c == FLUSH_BUFFER || buffer_index >= OUTPUT_BUFFER_SIZE) {
+	
+		write(STDERR_FILENO, output_buffer, buffer_index);
+		buffer_index = 0;
+	}
+
+	if (c != FLUSH_BUFFER)
+		output_buffer[buffer_index++] = c;
+
+	return 1;
+}
+/**
+ * custom_put_to_fd - writes a character to a specified file descriptor 
  * @c: the character to print
  * @fd: the file descriptor to write to
- * Return: On success 1
- * on error, -1 is returned, and errno is set appropriately.
+ * Return: 1 on success, -1 on error
  */
-int _putfd(char c, int fd)
-{
-	static int i;
-	static char buf[WRITE_BUF_SIZE];
+int custom_put_to_fd(char c, int fd) {
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-	{
-		write(fd, buf, i);
-		i = 0;
+	static int buffer_index;
+	static char output_buffer[OUTPUT_BUFFER_SIZE];
+
+	if (c == FLUSH_BUFFER || buffer_index >= OUTPUT_BUFFER_SIZE) {
+	
+		write(fd, output_buffer, buffer_index);
+		buffer_index = 0;
 	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
+	if (c != FLUSH_BUFFER)
+		output_buffer[buffer_index++] = c;
+
+	return 1;
 }
 /**
- * _putsfd - printing an input string
- * @str: the string to be printed
- * @fd: the file descriptor to write to
- * Return: the number of chars put
+ * custom_puts_to_fd - print a string to a specified file descriptor.
+ * @str: The string to be printed
+ * @fd: The file descriptor to write to
+ * Return: The number of characters written
  */
-int _putsfd(char *str, int fd)
-{
-	int i = 0;
+int custom_puts_to_fd(char *str, int fd) {
+
+	int char_count = 0;
 
 	if (!str)
-		return (0);
-	while (*str)
-	{
-		i += _putfd(*str++, fd);
+		return 0;
+	while (*str) {
+	
+		char_count += custom_put_to_fd(*str++, fd);
 	}
-	return (i);
+	return char_count;
 }
