825c626b622dd5d65f6dbeae4ffcdf54dc6042a1
Excelnoel <mannymacadex@gmail.com>
2023-11-13 05:59:05 -0800

    buildin

--- builtin.c
+++ builtin.c
@@ -19,7 +19,7 @@ int exitShell(info_t *info)
 
         if (info->argv[1])  /* If there is an exit argument */
         {
-                exitcheck = _erratoi(info->argv[1]);
+                exitcheck = _erratoi(info);
                 if (exitcheck == -1)
                 {
                         info->status = 2;
@@ -31,7 +31,7 @@ int exitShell(info_t *info)
                 info->err_num = _erratoi(info->argv[1]);
                 return (-2);
         }
-        info->err_num = -1;
+        info->err_num = _erratoi(info);
         return (-2);
 }
 
